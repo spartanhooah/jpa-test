8252fde01cf4817fcebff2107b318f956e23b6ba
Ben Frey <spartanhooah@gmail.com>
2024-05-19 08:48:23 -0400

    chore: remove extra directory nesting, remove extra db init script

--- src/main/java/net/frey/jpatest/bootstrap/BootstrapData.java
+++ src/main/java/net/frey/jpatest/bootstrap/BootstrapData.java
@@ -1,6 +1,5 @@
 package net.frey.jpatest.bootstrap;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.frey.jpatest.repository.ProblemzRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -12,8 +11,7 @@ public class BootstrapData implements CommandLineRunner {
     private final ProblemzRepository problemzRepository;
 
     @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-        problemzRepository.findAll();
+    public void run(String... args) {
+        System.out.println(problemzRepository.findAll());
     }
 }
